a6d5636e5076e560d106de09154f2046277af5bb
avidrucker <avidrucker@gmail.com>
2024-10-15 13:14:34 -0400

    fix width of canvas to not display offscreen

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import SkillTreeGraph from './SkillTreeGraph';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App w-100 h-100 overflow-hidden">
       <SkillTreeGraph />
     </div>
   );
